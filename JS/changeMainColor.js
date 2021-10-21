14c35902dcfdb8ba310bb34d49fa7cd887235d58
kheeyaa <kheeyaa@icloud.com>
2021-10-21 11:18:13 +0900

    feat: Update Colors type
    메인 색상의 종류를 더 늘림.

--- JS/changeMainColor.js
+++ JS/changeMainColor.js
@@ -1,6 +1,14 @@
 const changeMainColor = (() => {
-  //   const colors = ['#cdb4db', '#ffc8dd', '#ffafcc', '#bde0fe', '#a2d2ff']
-  const colors = ['#ef476f', '#ffd166', '#06d6a0', '#118ab2', '#073b4c'];
+  // const colors = ['#cdb4db', '#ffc8dd', '#ffafcc', '#bde0fe', '#a2d2ff'];
+  const colors = [
+    '#f5ffc6',
+    '#b4e1ff',
+    '#e4c1f9',
+    '#fface4',
+    '#c1ff9b',
+    '#ffe566',
+    '#f3d8c7'
+  ];
   const selectRandomColor = () =>
     colors[Math.floor(Math.random() * colors.length)];
 
